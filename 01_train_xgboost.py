4555b8928a66346f97096f2ba58b2a64dbd0b13c
jcwang587 <kashkingwong@gmail.com>
2025-10-01 20:56:50 -0400

    add target data

---
+++ 01_train_xgboost.py
@@ -0,0 +1,12 @@
+import xgboost as xgb
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import sys
+import warnings
+
+
+
+data = pd.read_csv('data/processed/data.csv')
+
